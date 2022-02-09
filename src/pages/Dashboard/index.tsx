cef686d8dc80f25c787afcebaacf84bc7916b717
Marcos CKL <marcosfraga@ckl.io>
2022-02-09 08:17:35 -0300

    ✨ create filter buttons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
-import SelectInput from '../../components/SelectInput';
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-    const months = [
-        { label: 'Janeiro', value: 'Janeiro' },
-        { label: 'Fevereiro', value: 'Fevereiro' },
-        { label: 'Março', value: 'Março' },
-        { label: 'Abril', value: 'Abril' },
-        { label: 'Maio', value: 'Maio' },
-        { label: 'Junho', value: 'Junho' },
-        { label: 'Julho', value: 'Julho' },
-        { label: 'Agosto', value: 'Agosto' },
-        { label: 'Setembro', value: 'Setembro' },
-        { label: 'Outubro', value: 'Outubro' },
-        { label: 'Novembro', value: 'Novembro' },
-        { label: 'Dezembro', value: 'Dezembro' },
-    ];
-
-    const years = [
-        { label: '2019', value: '2019' },
-        { label: '2020', value: '2020' },
-        { label: '2021', value: '2021' },
-        { label: '2022', value: '2022' },
-        { label: '2023', value: '2023' },
-    ];
-
     return (
         <Container>
             <ContentHeader title="Dashboard" lineColor="#d39cdf">
-                <SelectInput options={months} />
-                <SelectInput options={years} />
+                <div></div>
             </ContentHeader>
         </Container>
     );
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -2,30 +2,29 @@ import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import SelectInput from '../../components/SelectInput';
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
     const months = [
-        { label: 'Janeiro', value: 'Janeiro' },
-        { label: 'Fevereiro', value: 'Fevereiro' },
-        { label: 'Março', value: 'Março' },
-        { label: 'Abril', value: 'Abril' },
-        { label: 'Maio', value: 'Maio' },
-        { label: 'Junho', value: 'Junho' },
-        { label: 'Julho', value: 'Julho' },
-        { label: 'Agosto', value: 'Agosto' },
-        { label: 'Setembro', value: 'Setembro' },
-        { label: 'Outubro', value: 'Outubro' },
-        { label: 'Novembro', value: 'Novembro' },
-        { label: 'Dezembro', value: 'Dezembro' },
+        { label: 'Janeiro', value: '01' },
+        { label: 'Fevereiro', value: '02' },
+        { label: 'Março', value: '03' },
+        { label: 'Abril', value: '04' },
+        { label: 'Maio', value: '05' },
+        { label: 'Junho', value: '06' },
+        { label: 'Julho', value: '07' },
+        { label: 'Agosto', value: '08' },
+        { label: 'Setembro', value: '09' },
+        { label: 'Outubro', value: '10' },
+        { label: 'Novembro', value: '11' },
+        { label: 'Dezembro', value: '12' },
     ];
 
     const years = [
-        { label: '2019', value: '2019' },
-        { label: '2020', value: '2020' },
-        { label: '2021', value: '2021' },
         { label: '2022', value: '2022' },
-        { label: '2023', value: '2023' },
+        { label: '2021', value: '2021' },
+        { label: '2020', value: '2020' },
+        { label: '2019', value: '2019' },
     ];
 
     return (
@@ -34,6 +33,16 @@ const List: React.FC = () => {
                 <SelectInput options={months} />
                 <SelectInput options={years} />
             </ContentHeader>
+
+            <Filters>
+                <button type="button" className="tag-filter tag-filter-recurrent">
+                    Recorrentes
+                </button>
+
+                <button type="button" className="tag-filter tag-filter-eventual">
+                    Eventuais
+                </button>
+            </Filters>
 
             <Content>
                 <HistoryFinanceCard
