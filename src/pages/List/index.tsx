26e466b8d9495273b932bcf6ae69bea5de9663cc
Marcos CKL <marcosfraga@ckl.io>
2022-02-10 22:45:50 -0300

    ✨ implemented filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,3 +1,4 @@
+import UUID from 'pure-uuid';
 import React, { useEffect, useMemo, useState } from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
@@ -63,12 +64,25 @@ const List: React.FC<IRouteParams> = ({ match }) => {
         { label: 'Dezembro', value: 12 },
     ];
 
-    const years = [
-        { label: '2019', value: '2019' },
-        { label: '2020', value: '2020' },
-        { label: '2021', value: '2021' },
-        { label: '2022', value: '2022' },
-    ];
+    const years = useMemo(() => {
+        let uniqueYears: number[] = [];
+
+        listData.forEach(item => {
+            const date = new Date(item.date);
+            const year = date.getFullYear();
+
+            if (!uniqueYears.includes(year)) {
+                uniqueYears.push(year);
+            }
+        });
+
+        return uniqueYears.map(year => {
+            return {
+                value: String(year),
+                label: String(year),
+            };
+        });
+    }, [listData]);
 
     useEffect(() => {
         const filteredData = listData.filter(item => {
@@ -81,7 +95,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
 
         const dataList = filteredData.map(item => {
             return {
-                id: String(new Date().getTime()) + item.amount,
+                id: String(new UUID(1)),
                 description: item.description,
                 amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
